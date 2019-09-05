b04d5bd71e39aacb6fca4cc8bb56e6eb1dd46406
ak <ak@tangro.de>
2019-09-05 11:43:31 +0200

    initial commit

---
+++ src/audit/runAudit.ts
@@ -0,0 +1,74 @@
+import { exec } from '@actions/exec';
+import * as fs from 'fs';
+import path from 'path';
+import { Result } from '../Result';
+
+interface Audit {
+  metadata: {
+    vulnerabilities: {
+      info: number;
+      low: number;
+      moderate: number;
+      high: number;
+      critical: number;
+    };
+    dependencies: number;
+    devDependencies: number;
+    optionalDependencies: number;
+    totalDependencies: number;
+  };
+}
+
+const parseAudit = (audit: Audit): Result<Audit['metadata']> => {
+  const vulnerabilities = audit.metadata.vulnerabilities;
+  const sumOfVulnerabilities = Object.keys(vulnerabilities).reduce(
+    (sum, key) => sum + vulnerabilities[key],
+    0
+  );
+  const isOkay = vulnerabilities.high + vulnerabilities.critical === 0;
+  const shortText = `info: ${vulnerabilities.info} low: ${vulnerabilities.low} moderate: ${vulnerabilities.moderate} high: ${vulnerabilities.high} critical: ${vulnerabilities.critical}`;
+  const metadata = audit.metadata;
+  const text = `${sumOfVulnerabilities} ${
+    sumOfVulnerabilities === 1 ? 'vulnerability' : 'vulnerabilities'
+  } detected in ${audit.metadata.totalDependencies} total dependencies:
+  \ninfo: ${vulnerabilities.info}
+  \nlow: ${vulnerabilities.low}
+  \nmoderate: ${vulnerabilities.moderate}
+  \nhigh: ${vulnerabilities.high}
+  \ncritical: ${vulnerabilities.critical}`;
+
+  return {
+    metadata,
+    isOkay,
+    shortText,
+    text
+  };
+};
+
+export async function runAudit(): Promise<Result<Audit['metadata']>> {
+  let output = '';
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        output += data.toString();
+      }
+    }
+  };
+  await exec('npm', ['audit', '--json'], options);
+
+  try {
+    const auditResult: Audit = JSON.parse(output);
+    fs.mkdirSync('audit');
+    fs.writeFileSync(
+      path.join('audit', 'index.html'),
+      `<html><body><pre><code>${JSON.stringify(
+        auditResult,
+        null,
+        2
+      )}</code></pre></body></html>`
+    );
+    return parseAudit(auditResult);
+  } catch (error) {
+    throw error;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,62 @@
 import * as core from '@actions/core';
+import { GitHubContext, setStatus } from '@tangro/tangro-github-toolkit';
+import { runAudit } from './audit/runAudit';
+import { Result } from './Result';
+
+async function wrapWithSetStatus<T>(
+  context: GitHubContext,
+  step: string,
+  code: () => Promise<Result<T>>
+) {
+  setStatus({
+    context,
+    step,
+    description: `Running ${step}`,
+    state: 'pending'
+  });
+
+  try {
+    const result = await code();
+    setStatus({
+      context,
+      step,
+      description: result.shortText,
+      state: result.isOkay ? 'success' : 'failure'
+    });
+    return result;
+  } catch (error) {
+    setStatus({
+      context,
+      step,
+      description: `Failed: ${step}`,
+      state: 'failure'
+    });
+    core.setFailed(`CI failed at step: ${step}`);
+  }
+}
 
 async function run() {
   try {
-    if (!process.env.ENV_VARIABLE || process.env.ENV_VARIABLE.length === 0) {
+    if (
+      !process.env.GITHUB_CONTEXT ||
+      process.env.GITHUB_CONTEXT.length === 0
+    ) {
       throw new Error(
-        'You have to set the ENV_VARIABLE in your secrets configuration'
+        'You have to set the GITHUB_CONTEXT in your secrets configuration'
       );
     }
-    const context = JSON.parse(process.env.GITHUB_CONTEXT || '');
+    if (!process.env.GITHUB_TOKEN || process.env.GITHUB_TOKEN.length === 0) {
+      throw new Error(
+        'You have to set the GITHUB_TOKEN in your secrets configuration'
+      );
+    }
+    const context = JSON.parse(
+      process.env.GITHUB_CONTEXT || ''
+    ) as GitHubContext;
+
+    await wrapWithSetStatus(context, 'audit', async () => {
+      return await runAudit();
+    });
 
     core.debug('debug message');
   } catch (error) {
